fde1b441bf8ba99f74ccc502f4f11999e11b9b03
DonboyBR <donboymedeiros@gmail.com>
2025-06-26 11:53:59 -0300

    Criados pacotes controller, infrastructure, entitys, e repository

---
+++ cadastro-usuario/src/main/java/com/tribus/cadastro_usuario/business/UsuarioService.java
@@ -0,0 +1,8 @@
+package com.tribus.cadastro_usuario.business;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+}
---
+++ cadastro-usuario/src/main/java/com/tribus/cadastro_usuario/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,5 @@
+package com.tribus.cadastro_usuario.infrastructure.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+}
